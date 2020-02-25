99eb60a1a6332cdf6ab28272924ca031576519f3
seamo88 <bstockgame.88@gmail.com>
2020-02-25 14:13:56 -0600

    Add project files.

---
+++ 4_6_Lab_sean/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4_6_Lab_sean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday", "Sunday"};
+            string[] tasks =new string[7];
+
+            for (int i = 0; i < days.Length; i++)
+
+            {
+                Console.WriteLine("What are you going to do on " + days[i]);
+                tasks[i] = days[i] + ":" + Console.ReadLine();
+            }
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(tasks[i]);
+            }
+        }
+    }
+}
